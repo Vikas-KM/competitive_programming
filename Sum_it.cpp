9c1aec635b817de0ded3deb25583cf7af9958f4e
Vikas KM <vikki.km@gmail.com>
2023-02-01 01:13:12 +0530

    codechef practice programs

---
+++ Sum_it.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    int x, y, z;
+    while(t--){
+        cin >> x >> y >> z;
+        cout << (x + y == z ? "YES\n" : "NO\n");
+    }
+    return 0;
+}
---
+++ Waiting_Time.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    int x, y;
+    while(t--){
+        cin >> x >> y;
+        cout << (7 * x) - y << endl;
+    }
+    return 0;
+}
